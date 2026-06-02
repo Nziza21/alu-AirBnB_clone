7ad29086b5402626ac071c67566ae51eb8ebb4f9
Nziza21 <n.samuel@alustudent.com>
2026-06-02 13:15:04 +0200

    corrections

--- tests/test_models/test_amenity.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from models.amenity import Amenity
-
-
-class TestAmenity(unittest.TestCase):
-
-    def test_amenity(self):
-        a = Amenity()
-        self.assertTrue(hasattr(a, "name"))
